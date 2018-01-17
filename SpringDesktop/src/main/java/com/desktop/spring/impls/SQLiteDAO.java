29795fc96efa18a6b7fd6732a3e153597fd756d2
IJan <IJan@DESKTOP-AEOCCFV>
2018-01-17 21:53:22 +0100

    change

--- SpringDesktop/src/main/java/com/desktop/spring/impls/SQLiteDAO.java
+++ SpringDesktop/src/main/java/com/desktop/spring/impls/SQLiteDAO.java
@@ -108,7 +108,7 @@ public class SQLiteDAO implements ProfileDAO{
 	@Override
 	public void sendMessage(int sender_id, int recipient_id,String message) {
 		String sqlf = "insert into admin_spread.messages (sender_id,recipient_id,text,date) values (?,?,?,?)";
-		jdbcTemplate.update(sqlf,new Object[] {sender_id,recipient_id,message,LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)).toString()});
+		jdbcTemplate.update(sqlf,new Object[] {sender_id,recipient_id,message,LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)).toString()});
 	}
 	@Override
 	public List<MessageBody> getMesseges(int sender, int recipient) {
